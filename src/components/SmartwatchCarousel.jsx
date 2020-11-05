d75c63c66ebb93d165b1f737993da85414ce368f
Christian <christiandp7@gmail.com>
2020-11-04 21:42:17 -0400

    initialize Glide

--- src/components/SmartwatchCarousel.jsx
+++ src/components/SmartwatchCarousel.jsx
@@ -2,6 +2,21 @@ import React, { useEffect } from 'react'
 import Glide from '@glidejs/glide'
 
 function SmartwatchCarousel() {
+  useEffect(() => {
+    const glide = new Glidejs(".glide", {
+      peek: { before: 0, after: 70 },
+      perView: 3,
+      type: "carousel",
+      gap: 20,
+      //autoplay: 4000,
+      //focusAt: 3,
+      direction: 'rtl'
+    });
+    glide.mount();
+    return () => {
+      glide.destroy()
+    }
+  })
   return (
     <div>
       <h1>this is the carousel</h1>
