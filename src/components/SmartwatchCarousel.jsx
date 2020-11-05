f12202bb64377bd61896e75e0336c6f6fa3b1a91
Christian <christiandp7@gmail.com>
2020-11-05 11:34:18 -0400

    small bug in useEffect at SmartwatchCarousel.jsx

--- src/components/SmartwatchCarousel.jsx
+++ src/components/SmartwatchCarousel.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import Glide from '@glidejs/glide'
 
 function SmartwatchCarousel() {
-  useEffect(() => {
+  /*useEffect(() => {
     const glide = new Glidejs(".glide", {
       peek: { before: 0, after: 70 },
       perView: 3,
@@ -16,10 +16,17 @@ function SmartwatchCarousel() {
     return () => {
       glide.destroy()
     }
-  })
+  })*/
   return (
     <div>
-      <h1>this is the carousel</h1>
+      <div className="glide">
+        <div className="glide__slide">1</div>
+        <div className="glide__slide">2</div>
+        <div className="glide__slide">3</div>
+        <div className="glide__slide">4</div>
+        <div className="glide__slide">5</div>
+      </div>
+
     </div>
   )
 }
